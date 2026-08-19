567e7cfdec6307c1f7ed0d478a9a50a6b73b4cc2
nandisaiprakash6 <nandisaiprakash6@gmail.com>
2026-08-19 14:24:02 +0530

    added conclusions

---
+++ PRACTICAL-4/fatorial_iterative_conclusion.py
@@ -0,0 +1,15 @@
+
+# conclusion:-
+
+'''This program calculates the factorial of a given number n using iteration.
+
+Time Complexity: O(n) — because the loop executes n times.
+Space Complexity: O(1) — because it uses only a constant amount of extra memory.
+Approach: Iterative
+Advantage: Uses less memory than the recursive factorial approach.
+Final:
+
+Time = O(n)
+Space = O(1)
+
+So, for factorial calculation, this iterative approach is more memory-efficient than the recursive approach.'''
